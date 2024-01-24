c172e8d3d638a9d8797a147a48cf0863c4d73563
purple_flood <49845374+PurpleFlood@users.noreply.github.com>
2024-01-24 13:46:36 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
-echo "Downloading ghidra"
+echo "Downloading ghidra repo"
 git clone https://github.com/NationalSecurityAgency/ghidra.git
-echo "Downloading ida"
+echo "Downloading ida runtime"
 wget https://out7.hex-rays.com/files/idafree83_linux.run
 
 echo "creating out repo"
-mkdir $(pwd)/out/
+mkdir -p $(pwd)/out/
 mv idafree83_linux.run $(pwd)/out/
 mv -r ghidra/ $(pwd)/out/
